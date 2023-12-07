b0c2073956ce4aae3da22fda287058411df88707
Monica Villanueva <vllnv.monica@gmail.com>
2023-12-07 20:44:30 +0800

    initial commit

---
+++ app/src/main/java/com/villanueva/monica/block3/p1/quiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.villanueva.monica.block3.p1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
